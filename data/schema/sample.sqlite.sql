f06261051a92d7cc3aae3ee2cd74b0172979afeb
Jean-François Bilger <changi67@gmail.com>
2013-06-28 22:22:31 +0200

    [FIX] It's datetime and not date, #5

--- data/schema/sample.sqlite.sql
+++ data/schema/sample.sqlite.sql
@@ -8,10 +8,10 @@ INSERT INTO job (description,action,defaultschedule, estimatedduration)
     VALUES  ('cpuinfo',  'cat /proc/cpuinfo', '*/14 * * *', '02:00');
 
 INSERT INTO node (nodename, ipaddr, description, lastseen)
-    VALUES ('node1', '192.168.0.1', 'node 1', date('now'));
+    VALUES ('node1', '192.168.0.1', 'node 1', datetime('now'));
 INSERT INTO node (nodename, ipaddr, description, lastseen)
-    VALUES ('node2', '192.168.0.2', 'node 2', date('now'));
+    VALUES ('node2', '192.168.0.2', 'node 2', datetime('now'));
 INSERT INTO node (nodename, ipaddr, description, lastseen)
-  VALUES ('node3', '192.168.0.3', 'node 3', date('now'));
+  VALUES ('node3', '192.168.0.3', 'node 3', datetime('now'));
 INSERT INTO node (nodename, ipaddr, description, lastseen)
-  VALUES ('node4', '192.168.0.4', 'node 4', date('now'));
+  VALUES ('node4', '192.168.0.4', 'node 4', datetime('now'));
